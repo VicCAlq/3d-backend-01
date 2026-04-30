14a1f743e2eb17a07c9d03352ebb89cf22ae0f0f
lucasruan12 <lucas.ruan@nave.org.br>
2026-04-30 10:11:30 -0300

    Update Lucas Ruan - Biblioteca.sql

--- Lucas Ruan - Biblioteca.sql
+++ Lucas Ruan - Biblioteca.sql
@@ -4,27 +4,6 @@ nome VARCHAR(150) NOT NULL,
 materia VARCHAR(30)
 );
 
-CREATE TABLE IF NOT EXISTS turma(
-id_turma INTEGER PRIMARY KEY AUTOINCREMENT,
-nome_turma VARCHAR(2) NOT NULL,
-tipo VARCHAR(11)
-);
-
-CREATE TABLE IF NOT EXISTS alunos(
-id_aluno INTEGER PRIMARY KEY AUTOINCREMENT,
-nome VARCHAR(100) NOT NULL,
-id_turma INTEGER,
-FOREIGN KEY(id_turma) REFERENCES turma (id_turma)
-);
-
-CREATE TABLE IF NOT EXISTS notas(
-id_nota INTEGER PRIMARY KEY AUTOINCREMENT,
-id_aluno INTEGER,
-FOREIGN KEY(id_aluno) REFERENCES alunos(id_aluno),
-materia VARCHAR(900) NOT NULL,
-nota REAL
-);
-
 INSERT INTO professores (nome,materia)
 VALUES 
 ('Patricia Oliveira','Português'),
