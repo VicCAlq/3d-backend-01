7686f445f8ab0949403d6194c63f3b4491e0995d
lucasruan12 <lucas.ruan@nave.org.br>
2026-04-30 10:09:55 -0300

    Update Lucas Ruan - Biblioteca.sql

--- Lucas Ruan - Biblioteca.sql
+++ Lucas Ruan - Biblioteca.sql
@@ -1,15 +1,41 @@
-CREATE TABLE biblioteca (
-id INTEGER PRIMARY KEY AUTOINCREMENT,
-nome_do_livro VARCHAR(50) NOT NULL,
-categoria VARCHAR(50) NOT NULL,
-autor VARCHAR(100) NOT NULL,
-ano_de_lancamento INTEGER NOT NULL
+CREATE TABLE IF NOT EXISTS  professores (
+id_prof INTEGER PRIMARY KEY AUTOINCREMENT,
+nome VARCHAR(150) NOT NULL,
+materia VARCHAR(30)
 );
 
-INSERT INTO biblioteca (
-nome_do_livro,
-categoria,
-autor,
-ano_de_lancamento
-) VALUES
-('O Principe', "filosofia", "Nicolau Maquiavel", 1600);
+CREATE TABLE IF NOT EXISTS turma(
+id_turma INTEGER PRIMARY KEY AUTOINCREMENT,
+nome_turma VARCHAR(2) NOT NULL,
+tipo VARCHAR(11)
+);
+
+CREATE TABLE IF NOT EXISTS alunos(
+id_aluno INTEGER PRIMARY KEY AUTOINCREMENT,
+nome VARCHAR(100) NOT NULL,
+id_turma INTEGER,
+FOREIGN KEY(id_turma) REFERENCES turma (id_turma)
+);
+
+CREATE TABLE IF NOT EXISTS notas(
+id_nota INTEGER PRIMARY KEY AUTOINCREMENT,
+id_aluno INTEGER,
+FOREIGN KEY(id_aluno) REFERENCES alunos(id_aluno),
+materia VARCHAR(900) NOT NULL,
+nota REAL
+);
+
+INSERT INTO professores (nome,materia)
+VALUES 
+('Patricia Oliveira','Português'),
+('Amanda', 'Biologia'),
+('Juh Caldas', 'Design'),
+('Gabriel', 'Educação Física'),
+('Mateus Mendes', 'Matemática'),
+('Sheyla Mafra', 'História'),
+('Daniela', 'Geografia'),
+('Luana', 'Sociologia'),
+('Ivanildo', 'Química'),
+('Mateu Levy', 'Física'),
+('Glauco', 'Inglês'),
+('Victor Cavalcanti', 'Programação'),;
