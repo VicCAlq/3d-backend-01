8a7f84046b709d5201d4b9b8f297474427f460fd
Norcat27 <alan.silva@nave.org.br>
2026-05-02 16:19:23 -0300

    Update Alan Henrique - Biblioteca.sql

--- Alan Henrique - Biblioteca.sql
+++ Alan Henrique - Biblioteca.sql
@@ -9,4 +9,17 @@ VALUES
 ('3A','DESIGN'),
 ('3B', 'DESIGN'),
 ('3C', 'programação'),
-('3D', 'programação')
+('3D', 'programação');
+
+CREATE TABLE IF NOT EXISTS alunos (
+    id_aluno INTEGER PRIMARY KEY AUTOINCREMENT,
+    nome TEXT NOT NULL,
+    id_turma INTEGER NOT NULL,
+    FOREIGN KEY (id_turma) REFERENCES turma(id_turma));
+INSERT INTO alunos (nome, id_turma)
+VALUES
+('Beltran', 1),
+('Pablo Ruan', 1),
+('Isa Barros', 2),
+('Daniela', 3),
+('Alan Henrique', 4);
